2881738d9e737eb7dbd55c5ccdf1179faea69ec5
vchhu <90860763+vchhu@users.noreply.github.com>
2023-07-24 08:38:14 -0400

    Update anicafe.js

--- anicafe.js
+++ anicafe.js
@@ -27,12 +27,22 @@ function switchLang(lang) {
 function showMenu(n) {
 	var x = document.getElementById("meals");
 	var y = document.getElementById("drinks");
+	var z = document.getElementByClass("drink");
+	var m = document.getElementsByClassName("meal");
 	if (n == 0) {
 		x.classList.replace("d-none", "d-block");
 		y.classList.replace("d-block", "d-none");
+		z.classList.replace("d-block", "d-none");
+		for (let i = 0; i < m.length; i++) {
+			m[i].classList.replace("d-none", "d-block");
+		}
 	}
 	else if (n == 1) {
 		x.classList.replace("d-block", "d-none");
 		y.classList.replace("d-none", "d-block");
+		z.classList.replace("d-none", "d-block");
+		for (let i = 0; i < m.length; i++) {
+			m[i].classList.replace("d-block", "d-none");
+		}
 	}
 }

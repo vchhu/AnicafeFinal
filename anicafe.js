0dcad61a6c25ee59e8a55c79aaa97e6158f1412f
vchhu <90860763+vchhu@users.noreply.github.com>
2023-07-24 00:22:12 -0400

    Update anicafe.js

--- anicafe.js
+++ anicafe.js
@@ -4,6 +4,7 @@ function switchLang(lang) {
 	var nav3 = document.getElementById("navlink3");
 	var nav4 = document.getElementById("navlink4");
 	var nav5 = document.getElementById("navlink5");
+	var nav6 = document.getElementById("navlink6");
 	
 	if (lang == "en") {
 		nav1.innerHTML = "Contact Us";
@@ -11,7 +12,7 @@ function switchLang(lang) {
 		nav3.innerHTML = "View Menu";
 		nav4.innerHTML = "Shop For Merch";
 		nav5.innerHTML = "Book a Room or Table";
-		nav6.innerHTML = "Shopping Cart";
+		nav6.innerHTML = "Forums and Communities";
 	}
 	else if (lang == "fr") {
 		nav1.innerHTML = "Contactez-Nous";
@@ -19,5 +20,6 @@ function switchLang(lang) {
 		nav3.innerHTML = "Regarde le Menu";
 		nav4.innerHTML = "Achetez des Marchandises";
 		nav5.innerHTML = "Réserver une Chambre ou une Table";
+		nav6.innerHTML = "Forums et Communautés";
 	}
 }
